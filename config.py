36a31ea6a4bd616b66c0610c58b928235c34f985
StreetWanderer <nicolas@streetwanderer.com>
2015-10-23 15:40:54 -0400

    Generate gif from images using imageio

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 DSCOVR_BASE_URL = 'http://epic.gsfc.nasa.gov'
 DSCOVR_API_PATH = '/api/images.php'
 DSCOVR_IMG_PATH = '/epic-archive/{format}/{image}.{format}'
+
+GIF_PATH = './test.gif'
--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import imageio
 import json, requests
 import config
 
@@ -7,31 +8,40 @@ def getImagesInfo(date):
     path = config.DSCOVR_API_PATH.format(date=date)
     path = config.DSCOVR_BASE_URL + path
 
+    #return ['epic_1b_20151022005948_00', 'epic_1b_20151022024751_00', 'epic_1b_20151022081200_00', 'epic_1b_20151022100003_00', 'epic_1b_20151022114806_00', 'epic_1b_20151022133609_00', 'epic_1b_20151022152413_00', 'epic_1b_20151022171216_00', 'epic_1b_20151022190018_00']
 
     imagesList = requests.get(config.DSCOVR_BASE_URL + config.DSCOVR_API_PATH, params={'date':date})
     jsonImages = json.loads(imagesList.text)
 
     parsedData = []
     for img in jsonImages:
-        parsedImages.append(img['image'])
+        parsedData.append(img['image'])
 
     return parsedData;
 
 def downloadImages(imageNames, format):
-    gifBytes = []
+    #Download the given DSCOVR images as a byteArray
+    imageFrames = []
     for img in imageNames:
         path = config.DSCOVR_BASE_URL + config.DSCOVR_IMG_PATH.format(format=format, image=img)
-        imgBytes = requests.get(path)
-        gifBytes.append(imgBytes.content)
+        #path = "http://127.0.0.1:1026/"+img+".png"
+        img = imageio.imread(path)
+        imageFrames.append(img)
 
-    return gifBytes
+    return imageFrames
 
 
+#Find the images for yesterday
+yesterday = (date.today() - timedelta(1)).strftime('%Y%m%d')
+print "getting images for {date}".format(date=yesterday)
+imageData = getImagesInfo(yesterday)
+gifFramesArray = downloadImages(imageData, 'png')
+print "obtained {x} images".format(x=len(gifFramesArray))
 
+#Generate the gif based on yesterday images
+print "generating gif..."
+imageio.mimwrite(config.GIF_PATH, gifFramesArray, loop=0, duration=0.6, quantizer='wu', subrectangles=True)
+print "gif saved to {gif}".format(gif=config.GIF_PATH)
 
-yesterday = date.today() - timedelta(1)
-imageData = getImagesInfo(yesterday.strftime('%Y%m%d'))
-gifBytesArray = downloadImages(imageData, 'png')
-#transform gifBytesArray to .gif
 #get yesterday headline from TheGuardian
 #combine the gif and headlines to upload to Tumblr
