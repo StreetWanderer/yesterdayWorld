b99da71d1df9505278385e7beea8b654a47e321a
StreetWanderer <nicolas@streetwanderer.com>
2015-10-29 14:17:50 -0400

    config cleanup

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ DSCOVR_BASE_URL = 'http://epic.gsfc.nasa.gov'
 DSCOVR_API_PATH = '/api/images.php'
 DSCOVR_IMG_PATH = '/epic-archive/{format}/{image}.{format}'
 
-GUARDIAN_APIKEY = ''
 #GUARDIAN
+GUARDIAN_APIKEY = ''
 GUARDIAN_REQUEST = 'http://content.guardianapis.com/world?section=world&from-date={date}&to-date={date}&order-by=relevance&use-date=newspaper-edition&show-fields=trailText%2Cheadline%2CnewspaperPageNumber%2Cstandfirst&page-size=100&api-key={api_key}'
 
 #TUMBLR
@@ -21,4 +21,3 @@ TWITTER_OAUTH_SECRET = ''
 
 #SYSTEM
 GIF_PATH = './earth.gif'
-GIF_PATH_NODATA = './noData.gif'
